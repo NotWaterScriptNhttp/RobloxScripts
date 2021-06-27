535a832700621a0a2bad5e8b5518d12a9cbd88bd
NotWaterScriptNhttp <68260488+NotWaterScriptNhttp@users.noreply.github.com>
2021-06-28 01:11:31 +0200

    Update UIModule.lua

--- Modules/UIModule.lua
+++ Modules/UIModule.lua
@@ -1,1 +1,9 @@
---soon
+function comment(...)end
+
+comment("You can use this module in anyway. Find more at https://github.com/NotWaterScriptNhttp/RobloxScripts")
+
+local module = {}
+
+--Soon y'all need to wait a bit
+
+return module
