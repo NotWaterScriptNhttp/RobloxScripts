97f6fb0be56c3a2a7d55ac2447a10e76546a6e58
NotWaterScriptNhttp <68260488+NotWaterScriptNhttp@users.noreply.github.com>
2021-06-29 00:05:02 +0200

    Added Utils and CreateButton

--- Modules/UIModule.lua
+++ Modules/UIModule.lua
@@ -3,6 +3,34 @@ function comment(...)end
 comment("You can use this module in anyway. Find more at https://github.com/NotWaterScriptNhttp/RobloxScripts")
 
 local module = {}
+local Utils = {}
+
+----------------------------
+------------Utils-----------
+----------------------------
+
+
+function Utils.OffsetColor(col,r,g,b,op)
+	col = col or Color3.new(0,0,0)
+	
+	op = op or "p"
+	
+	r = r or 0
+	g = g or 0
+	b = b or 0
+	
+	if op == "p" then
+		return Color3.fromRGB((col.R * 255) + r,(col.G * 255) + g,(col.B * 255) + b)
+	elseif op == "n" then
+		return Color3.fromRGB((col.R * 255) - r,(col.G * 255) - g,(col.B * 255) - b)
+	else
+		return Color3.fromRGB((col.R * 255) + r,(col.G * 255) + g,(col.B * 255) + b)
+	end
+end
+
+----------------------------
+------------UIlib-----------
+----------------------------
 
 function module.CreateLabel(parent,pos,size,text,spoiler)
 	assert(typeof(parent) == "Instance","Argument #1 must be 'Instance', got '"..typeof(parent).."'")
@@ -58,15 +86,38 @@ function module.CreateLabel(parent,pos,size,text,spoiler)
 			end
 		end)
 	end
+	return LabelBackground
 end
 
-function module.CreateButton(parent,pos,size,text,func)
+function module.CreateButton(parent,pos,size,text,color,func)
 	assert(typeof(parent) == "Instance","Argument #1 expected 'Instance', got '"..typeof(parent).."'")
 	assert((typeof(pos) == "UDim2" and typeof(size) == "UDim2") == true,"Pos or Size expected 'UDim2', got (Pos: "..typeof(pos)..", Size: "..typeof(size)..")")
 	assert(typeof(text) == "string","Argument #4 expected 'string', got '"..typeof(text).."'")
-	assert(typeof(func) == "function","Arguments #5 expected 'function', got '"..typeof(func).."'")
+	assert(typeof(color) == "Color3","Arguments #5 expected 'Color3', got '"..typeof(color).."'")
+	assert(typeof(func) == "function","Arguments #6 expected 'function', got '"..typeof(func).."'")
 	
+	local ButtonBack = Instance.new("TextLabel",parent)
+	ButtonBack.BorderSizePixel = 0
+	ButtonBack.Size = size
+	ButtonBack.Position = pos
+	ButtonBack.BackgroundColor3 = color
 	
+	ButtonBack.Text = text
+	ButtonBack.TextScaled = true
+	
+	ButtonBack.MouseEnter:Connect(function()
+		ButtonBack.BackgroundColor3 = Utils.OffsetColor(color,100,100,100,"p")
+	end)
+	ButtonBack.InputBegan:Connect(function(key)
+		if key.UserInputType  == Enum.UserInputType.MouseButton1 then
+			func()
+		end
+	end)
+	ButtonBack.MouseLeave:Connect(function()
+		ButtonBack.BackgroundColor3 = color
+	end)
+	
+	return ButtonBack
 end
 
 return module
