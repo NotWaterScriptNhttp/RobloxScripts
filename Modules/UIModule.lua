b3eaf981461d299c83ab48b27487af6954b1f98c
NotWaterScriptNhttp <68260488+NotWaterScriptNhttp@users.noreply.github.com>
2021-06-28 20:30:00 +0200

    Added CreateTable

--- Modules/UIModule.lua
+++ Modules/UIModule.lua
@@ -4,6 +4,69 @@ comment("You can use this module in anyway. Find more at https://github.com/NotW
 
 local module = {}
 
---Soon y'all need to wait a bit
+function module.CreateLabel(parent,pos,size,text,spoiler)
+	assert(typeof(parent) == "Instance","Argument #1 must be 'Instance', got '"..typeof(parent).."'")
+	assert((typeof(pos) == "UDim2" and typeof(size) == "UDim2") == true,"Pos or Size expected 'UDim2', got (Pos: "..typeof(pos)..", Size: "..typeof(size)..")")
+	assert(typeof(text) == "string","Argument #4 ")
+	local LabelBackground = Instance.new("Frame",parent)
+	LabelBackground.Size = size
+	LabelBackground.Position = pos
+	
+	LabelBackground.BorderSizePixel = 0
+	LabelBackground.BackgroundColor3 = Color3.fromRGB(90, 90, 90)
+	
+	local Label = Instance.new("TextLabel",LabelBackground)
+	Label.Size = UDim2.new(1,0,1,0)
+	Label.Position = UDim2.new(0,0,0,0)
+	Label.TextScaled = true
+	Label.TextWrapped = true
+	Label.BorderSizePixel = 0
+	Label.TextColor3 = Color3.fromRGB(255, 255, 255)
+	Label.Text = text
+
+	if typeof(spoiler) == "boolean" and spoiler == true then
+		local Spoiler = Instance.new("Frame",Label)
+		Spoiler.BorderSizePixel = 0
+		Spoiler.Size = UDim2.new(1,0,1,0)
+		Spoiler.Position = UDim2.new(0,0,0,0)
+		
+		Spoiler.BackgroundColor3 = Color3.new(0.137255, 0.137255, 0.137255)
+		
+		local SpoilerLabel = Instance.new("TextLabel",Spoiler)
+		SpoilerLabel.Size = UDim2.new(1,0,1,0)
+		SpoilerLabel.Position = UDim2.new(0,0,0,0)
+		SpoilerLabel.TextScaled = true
+		SpoilerLabel.TextColor3 = Color3.new(1,1,1)
+		SpoilerLabel.Text = "Spoiler"
+		SpoilerLabel.BackgroundTransparency = 1
+		
+		local active = false
+		
+		Spoiler.InputBegan:Connect(function(key)
+			if key.UserInputType == Enum.UserInputType.MouseButton1 then
+				active = not active
+				SpoilerLabel.Visible = not SpoilerLabel.Visible
+				if active == true then
+					Spoiler.BackgroundTransparency = 0.7
+					Spoiler.BackgroundColor3 = Color3.new(1,1,1)
+					Label.TextColor3 = Color3.new(0,0,0)
+				else
+					Spoiler.BackgroundTransparency = 0
+					Spoiler.BackgroundColor3 = Color3.new(0.137255, 0.137255, 0.137255)
+					Label.TextColor3 = Color3.new(1,1,1)
+				end
+			end
+		end)
+	end
+end
+
+function module.CreateButton(parent,pos,size,text,func)
+	assert(typeof(parent) == "Instance","Argument #1 expected 'Instance', got '"..typeof(parent).."'")
+	assert((typeof(pos) == "UDim2" and typeof(size) == "UDim2") == true,"Pos or Size expected 'UDim2', got (Pos: "..typeof(pos)..", Size: "..typeof(size)..")")
+	assert(typeof(text) == "string","Argument #4 expected 'string', got '"..typeof(text).."'")
+	assert(typeof(func) == "function","Arguments #5 expected 'function', got '"..typeof(func).."'")
+	
+	
+end
 
 return module
